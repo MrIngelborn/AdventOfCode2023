f05b334a0e624ecf74206694421f73ea86ecd471
mringelborn <mringelborn@protonmail.com>
2023-12-04 22:00:37 +0100

    Day 4: Add implementation of instance counting

--- Day 4/src/CardCollection.kt
+++ Day 4/src/CardCollection.kt
@@ -1,9 +1,17 @@
+import kotlin.math.max
+import kotlin.math.min
+
 class CardCollection (val cards: List<Card>) {
     val instancesSum: Int
         get() = cards.map(Card::instances).sum()
 
     fun playGame() {
-        TODO("Not yet implemented")
+        cards.forEachIndexed { cardIndex, card ->
+            val matchingNumbers = card.getWinningNumbersYouHave().size
+            for (i in cardIndex+1..min(cardIndex+matchingNumbers, cards.size-1)) {
+                cards[i].instances += card.instances
+            }
+        }
     }
 
     companion object Factory {
