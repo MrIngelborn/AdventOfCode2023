cb2846be49a20f9cef5b9bd2db55b916496aaf6b
mringelborn <mringelborn@protonmail.com>
2023-12-06 20:33:15 +0100

    Day 6: Finish task 1

---
+++ Day 6/src/Race.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+
+private const val inputFileName = "input.txt"
+
+private fun getInputLines() =
+    object {}.javaClass.getResource(inputFileName)?.toURI()!!.let(::File).readLines()
+
+fun main() {
+    val timeSheet = TimeSheet.create(getInputLines())
+    timeSheet.numberOfWaysToWinProduct.let(::println)
+}
