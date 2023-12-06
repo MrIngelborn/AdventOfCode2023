e8b71cf949e7b021051bbfdb704b16b41be13f6c
mringelborn <mringelborn@protonmail.com>
2023-12-06 20:20:55 +0100

    Day 6: Fix implementation

--- Day 6/src/TimeSheet.kt
+++ Day 6/src/TimeSheet.kt
@@ -1,13 +1,43 @@
-class TimeSheet {
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
-    val timeDistancePairs: Set<Pair<Int,Int>>
-        get() = setOf()
+class TimeSheet(val timeDistancePairs: Set<Pair<Int, Int>>) {
+
     companion object Factory {
+        private val lineRegex = Regex("\\w+:(.*)")
+        private val numberRegex = Regex("\\d+")
         fun create(lines: List<String>): TimeSheet {
-            return TimeSheet()
+            val timeList: List<Int> = parseNumbers(lines[0])
+            val distanceList: List<Int> = parseNumbers(lines[1])
+
+            return TimeSheet(timeList.zip(distanceList).toSet())
         }
+
+        private fun parseNumbers(line: String): List<Int> =
+            lineRegex.find(line)!!.groups[1]!!.value
+                .let { numberRegex.findAll(it) }
+                .map(MatchResult::value)
+                .map(String::toInt)
+                .toList()
     }
+
     fun numberOfWaysToWin(pair: Pair<Int, Int>): Int {
-        return 0
+        val roots = solveForRealRoots(-1.0, pair.first.toDouble(), -pair.second.toDouble())!!.let(::roundPairToInt)
+        return roots.second - roots.first + 1
     }
+
+    private fun solveForRealRoots(a: Double, b: Double, c: Double): Pair<Double, Double>? {
+        val discriminant = b * b - 4 * a * c
+        if (discriminant < 0) {
+            return null
+        }
+        val root1 = (-b + sqrt(discriminant)) / (2 * a)
+        val root2 = (-b - sqrt(discriminant)) / (2 * a)
+        return Pair(root1, root2)
+    }
+
+    private fun roundPairToInt(pair: Pair<Double, Double>): Pair<Int, Int> =
+        pair.first.inc().toInt() to pair.second.minus(0.0001).toInt()
+
+
 }
