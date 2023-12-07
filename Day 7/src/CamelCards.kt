e3024c1d38c2ade7c7ee5967f4ad83b48f74e861
mringelborn <mringelborn@protonmail.com>
2023-12-07 21:51:49 +0100

    Day 7: Parse input

--- Day 7/src/CamelCards.kt
+++ Day 7/src/CamelCards.kt
@@ -1,8 +1,17 @@
 class CamelCards(val handToBidPairs: List<Pair<Hand, Int>>) {
 
     companion object Factory {
+        private val lineRegex = Regex("([\\dTJQKA]+)\\s+(\\d+)")
         fun create(lines: List<String>): CamelCards {
-            return CamelCards(listOf())
+            val handToBidPairs = lines.map { line ->
+                lineRegex.find(line)?.groupValues?.let { values ->
+                    val hand: Hand = Hand.create(values[1])
+                    val bet = values[2].toInt()
+                    hand to bet
+                }
+            }
+
+            return CamelCards(handToBidPairs.filterNotNull())
         }
     }
 
--- Day 7/src/Hand.kt
+++ Day 7/src/Hand.kt
@@ -1,4 +1,7 @@
 class Hand(val cards: List<Card>) : Comparable<Hand> {
+    companion object Factory {
+        fun create(string: String) = Hand(string.map(::Card))
+    }
     override fun compareTo(other: Hand): Int {
         TODO("Not yet implemented")
     }
