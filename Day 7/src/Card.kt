8abcf84c57d2ebd5e460f1747d81c3c7968ec6c1
mringelborn <mringelborn@protonmail.com>
2023-12-07 21:58:24 +0100

    Day 7: Sort cards

--- Day 7/src/Card.kt
+++ Day 7/src/Card.kt
@@ -1,5 +1,29 @@
+import java.util.Objects
+
 class Card(val value: Char) : Comparable<Card> {
     override operator fun compareTo(other: Card): Int {
-        return 0
+        return intValue.compareTo(other.intValue)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Card)  {
+            return Objects.equals(value, other.value)
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+    private val intValue
+        get() = when (value) {
+            in '2'..'9' -> value-'0'
+            'T' -> 10
+            'J' -> 11
+            'Q' -> 12
+            'K' -> 13
+            'A' -> 14
+            else -> 0
+        }
 }
