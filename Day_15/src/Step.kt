62b615c22634371d71e94dffd589ca529fc33eae
mringelborn <mringelborn@protonmail.com>
2023-12-15 15:46:59 +0100

    Day 15: Parse step

--- Day_15/src/Step.kt
+++ Day_15/src/Step.kt
@@ -1,8 +1,20 @@
-class Step(private val string: String, val operation: Any, val label: Any, val focalLength: Int?) {
+class Step(val operation: Operation, val label: String, val focalLength: Int?) {
 
     companion object Factory {
-        fun create(string: String) : Step {
-            TODO()
+        private val addOperationRegex = Regex("(\\w+)=(\\d)")
+        private val deleteOperationRegex = Regex("(\\w+)-")
+        fun create(string: String): Step = when {
+            addOperationRegex.matches(string) -> {
+                val groupValues = addOperationRegex.find(string)!!.groupValues
+                Step(Operation.ADD, groupValues[1], groupValues[2].toInt())
+            }
+
+            deleteOperationRegex.matches(string) -> {
+                val groupValues = deleteOperationRegex.find(string)!!.groupValues
+                Step(Operation.DELETE, groupValues[1], null)
+            }
+
+            else -> throw IllegalArgumentException("Could not parse string to Step")
         }
     }
 
@@ -10,6 +22,9 @@ class Step(private val string: String, val operation: Any, val label: Any, val f
         get() = Hasher.hash(this.toString())
 
     override fun toString(): String {
-        return string
+        return label + when (operation) {
+            Operation.ADD -> "=$focalLength"
+            Operation.DELETE -> "-"
+        }
     }
 }
