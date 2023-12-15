2cfc8738fe644c99d80ba090678f6988aa0185a7
mringelborn <mringelborn@protonmail.com>
2023-12-15 14:19:50 +0100

    Day 15: Implement hashing

--- Day_15/src/Hasher.kt
+++ Day_15/src/Hasher.kt
@@ -2,15 +2,17 @@ class Hasher(val strings: List<String>) {
 
     companion object Factory {
         fun parseLines(lines: List<String>): Hasher {
-            TODO()
+            val strings = lines.flatMap { line ->  line.split(',') }
+            return Hasher(strings)
         }
     }
 
-    fun hash(string: String): Int {
-        TODO()
-    }
+    fun hash(string: String): Int =
+        string.fold(0) { acc, char ->
+            ((acc + char.code) * 17) % 256
+        }
 
     val hashSum: Int
-        get() = TODO()
+        get() = strings.map(::hash).sum()
 
 }
