8ff0b41d58a1e054c209ee046034597df0b268c1
mringelborn <mringelborn@protonmail.com>
2023-12-16 13:54:01 +0100

    Day 15: Finish part 2

--- Day_15/src/LensLibrary.kt
+++ Day_15/src/LensLibrary.kt
@@ -2,4 +2,8 @@ fun main() {
     val inputParser = InputParser("input.txt")
     val initSequence = InitSequence.parseLines(inputParser.lines)
     initSequence.hashSum.let(::println)
+
+    val boxes = Boxes()
+    boxes.applySteps(initSequence.steps)
+    boxes.focusPower.let(::println)
 }
